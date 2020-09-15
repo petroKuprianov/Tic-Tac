0dc1e0f244661b2caa79dc87023b057c3c17b3dd
petroKuprianov <petrokuprianov@gmail.com>
2020-09-15 13:36:36 +0300

    add complicated computer

--- public/js/scriptComp.js
+++ public/js/scriptComp.js
@@ -1,6 +1,6 @@
+var a = $(".game .item");
 jQuery(function(){
     $ = $ || jQuery;
-    var a = $(".game .item");
     var fields; 
     var firstStep; 
     var winer = {
@@ -43,10 +43,10 @@ jQuery(function(){
      }
     });
 
-   clickComp(fields);
+  //  clickComp(fields);
+  step(fields,1,clickComp,checkWin,clickGamer,symbol);
    if ( checkWin(fields,2) ) {
      $("#game-krestiki-noliki .win").show();
-
      winer.you+=1;
    } else
    if ( checkWin(fields,1) ) {
@@ -144,4 +144,60 @@ jQuery(function(){
     for(var p in arr)
      tmp[p] = arr[p];
      return tmp;
+    }
+
+
+    function step(board,symbol,clickComp,checkWin, clickGamer,symbol){
+      console.log(board);
+      const count = board.length;
+  
+      // Приоритет хода.В зависмости от него компьютер обороняется/атакует 
+      let priority = 0;
+      // Индексы ячейки, которую нужно заполнить в завсимсоти от приоритета
+      let px, py; 
+  
+      for (let i = 0; i <= count; i++) {
+        console.log("inside 1-st for");
+          if (board[i] === 0) {
+            console.log("inside first if");
+            // Имитируем ход компьютера
+            board[i]= symbol;
+            // Если ход компьютра победный, то сохраняем индексы ячейки
+            if (checkWin(board, symbol)) {
+              console.log("inside second if");
+              px = i;
+              priority = 2;
+              break;
+            }
+            // Имитируем ход пользователя
+            board[i] = 2;
+            // Если ход компьютра победный, то сохраняем индексы ячейки
+            if (checkWin(board, 2)) {
+              console.log("inside third if");
+              px = i;
+              priority = 1;
+            }
+            // Очищаем ячейку после симулированных ходов
+            board[i] = 0;
+          }
+        // Если у комьютера есть возможность выиграть, то выходим из цикла
+        if (priority === 2){
+          console.log("inside if break");
+          break;
+        }
+      }
+      // Если приоритет не изменился, то компьютер ходит случайно
+      if (priority === 0) {
+        clickComp(board);
+        console.log("click comp");
+      } else {
+        board[px] = 1;
+        console.log("click step");
+        a.each(function(i,e){
+        if( i == px ) {
+        $(e).off("click",clickGamer);
+        $(e).html(symbol(1));
+        }
+      });
+      }
     }
